c050f4ed6c351d5d2b7a3491a8408eefdce47996
Bai Omar <bai.laye22@bathspa.ac.uk>
2023-12-08 12:00:58 +0000

    dunnpo made it better ig... still needs work

--- Databases - The Big One/mYtHINGS/WIP.cpp
+++ Databases - The Big One/mYtHINGS/WIP.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 
 int main() {
+    while (true) {
     string SUB = "SUBSTITUTE(";
     string Output = "=";
-    string RangeCompare, RangeReplace, RangeTarget; // compare it what its being compared to, replace is what values to replace with, target is
-    // string UserInput = "D3:D14,B3:B6,A3:A6"
+    string RangeCompare, RangeReplace, RangeTarget; // compare is the range of values its being compared to, replace is what values to replace with, target is the range of values to replace
     
+    cout << "Input Data in format [ =(Target Range), (Compare Range), (Replace Range) ] where:\nTarget Range - The range of values that need to be Replaced\nCompare Range - The range of values \'Target Range\' is being Compared to\nReplace Range - The range of values that are being Substituted In if found (Range needs to be the same length as compare)\n\nInput Data: ";
     getline(cin, RangeTarget, ',');
     
     if(RangeTarget[0] == '=')
@@ -19,53 +20,57 @@ int main() {
     //second bit
     getline(cin, RangeCompare, ',');
     
-    string CompStart = "", CompEnd = "", TempNum = ""; //Range Start to range end strings
+    string CompStr = "", TempNum = ""; //Range string - letter coordinate
     bool usingCompStart = true; 
-    int iCompStart, iCompEnd; //Range Start to range end integers
+    int RCStart, RCEnd; //Start and End integers - replace compare start
     
     for (int i = 0; i < RangeCompare.length(); i++) {
         if (RangeCompare[i] == ':') {
-            iCompStart = stoi(TempNum);
+            RCStart = stoi(TempNum);
             TempNum = "";
+            usingCompStart = false;
             continue;
         }
-        else if (RangeCompare[i] >= 'A' && RangeCompare[i] <= 'Z'){
-            (usingCompStart) ? CompStart += RangeCompare[i] : CompEnd += RangeCompare[i];
+        else if (usingCompStart && RangeCompare[i] >= 'A' && RangeCompare[i] <= 'Z'){
+            CompStr += RangeCompare[i];
         } else if (RangeCompare[i] >= '0' && RangeCompare[i] <= '9'){
-            TempNum += RangeCompare; 
+            TempNum += RangeCompare[i];
         }
-        if (RangeCompare.length() == i-1 && usingCompStart == false){
-            iCompEnd = stoi(TempNum);
+        if (RangeCompare.length() == i+1 && usingCompStart == false){
+            RCEnd = stoi(TempNum);
         }
     }
     
     //third bit
-    string RepStart = "", RepEnd = ""; //Range Start to range end strings
-    bool usingRepStart = true; 
-    int iRepStart, iRepEnd;
-    TempNum = "";
+    string RepStr = ""; //Range Start to range end strings
+    bool usingRepStart = true;
     
     getline(cin, RangeReplace);
     
     for (int i = 0; i < RangeReplace.length(); i++) {
-        if (RangeReplace[i] == ':') {
-            iRepStart = stoi(TempNum);
-            TempNum = "";
-            continue;
-        }
-        else if (RangeReplace[i] >= 'A' && RangeReplace[i] <= 'Z'){
-            (usingRepStart) ? RepStart += RangeCompare[i] : RepEnd += RangeCompare[i];
+        if (RangeReplace[i] >= 'A' && RangeReplace[i] <= 'Z'){
+            RepStr += RangeReplace[i];
         }
         else if (RangeReplace[i] >= '0' && RangeReplace[i] <= '9'){
-            TempNum += RangeReplace; 
-        }
-        if (RangeReplace.length() == i-1 && usingRepStart == false){
-            iRepEnd = stoi(TempNum);
+            break;
         }
     }
+    
+    for (int i = RCStart; i <= RCEnd; i++) Output += SUB;
+    Output += (RangeTarget + ',');
+    for (int i = RCEnd; i >= RCStart; i--) {
+        string CurrIt = to_string(i);
+        
+        Output += (CompStr + CurrIt + ',');
+        Output += (RepStr + CurrIt + "),");
+    }
+    Output.erase(Output.length()-1,1);
     
     // input format
     //=D3:D14,B3:B6,A3:A6
     // output format
     //=SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(SUBSTITUTE(D3:D14,B3,A3),B4,A4),B5,A5),B6,A6)
+    cout << "reached end :D" << endl;
+    cout << Output << "\n\n\n" << endl;
+    }
 }
